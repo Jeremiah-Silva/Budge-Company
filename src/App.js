8dd90e54b3b9b86dfa4992a7c803bc098ef88b10
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-06-30 17:20:47 -0600

    Updating Modules

--- src/App.js
+++ src/App.js
@@ -30,28 +30,32 @@ function App() {
             title={expenses[0].title} 
             amount={expenses[0].amount} 
             date={expenses[0].date}
-            >
-            </ExpenseItem>
+            />
+          
             <ExpenseItem 
             title={expenses[1].title} 
             amount={expenses[1].amount} 
             date={expenses[1].date}
-            >
-            </ExpenseItem>
+            />
+         
             <ExpenseItem 
             title={expenses[2].title} 
             amount={expenses[2].amount} 
             date={expenses[2].date}
-            >
-            </ExpenseItem>
+            />
+            
             <ExpenseItem 
             title={expenses[3].title} 
             amount={expenses[3].amount} 
             date={expenses[3].date}
-            >
-            </ExpenseItem>
+            />
+          
           </div>
           );
 }
 
 export default App;
+
+//There is no a hard rule how to split.
+//Props are our way to passing data from component A to B. 
+//pass data for our component to direct child component.
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,43 +1,22 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    /** Month - props.date.toLocaleString(we can put two arguments - 1) Language 2)Date should be formated "  ")
-     We can define the size of the month display using for example 'long'*/
-    const month = props.date.toLocaleString('en-US', { month: 'long' })
-    /** How to setup the day of month propety */
-    const day = props.date.toLocaleString('en-US', { day: '2-digit' })
-    /** How to setup the year with 4 digits */
-    const year = props.date.getFullYear()
-
-
- //Aplying a dynaminc written in JavaScript
-  return (
-    <div>
+/** We moved these const to the file ExpenseDate.js */
+//Aplying a dynaminc written in JavaScript
+  
+  return(
       <div className="expense-item">
-      {/**The way to adapted props instead of expenseDate.toISOString */}
-      {/** <div>{props.date.toISOString()} </div>*/}
-      
-      {/** To maintaing the code cleaned we need to put it on const and apply this function to our <div>  */}
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
-    
-      {/** developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString */}
-      
-
-       <div className="expense-item__description">  
-
+        <ExpenseDate date={props.date} /> 
+        <div className="expense-item__description">  
         {/*<h2>{*instead of expenseTitle we put props}</h2>*/}
         <h2>{props.title}</h2> 
-        {/*When we chance the .title to .name we need to chance de name of attribute in App.js line 30 */}
-
+       {/*When we chance the .title to .name we need to chance de name of attribute in App.js line 30 */}
        {/**The way to adapted props instead of expenseAmount.toISOString */}
-       <div className="expense-item__price">${props.amount}
-            </div>
-          </div>
-        </div>
-     </div>
-    );
+       <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;

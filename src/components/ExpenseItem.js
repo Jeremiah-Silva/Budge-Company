c875b630908958a102bcb889e2c954f1da79d1c0
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-06-30 11:36:51 -0600

    Working with props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,30 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
- const expenseDate = new Date(2022, 5, 29);
- // This const is applied at line number 14.
- const expenseTitle = 'Car Insurace'; 
- const expenseAmount = 295.79;
+function ExpenseItem(props) {
 
  //Aplying a dynaminc written in JavaScript
   return (
     <div>
-     <div><h1>Company Budget</h1></div>
       <div className="expense-item">
-       <div>{expenseDate.toISOString()} </div>
-        <div className="expense-item__description">   
-         <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
-       </div>
-      </div>
+
+       {/**The way to adapted props instead of expenseDate.toISOString */}
+       <div>{props.date.toISOString()} </div>
+
+       <div className="expense-item__description">  
+
+        {/*<h2>{*instead of expenseTitle we put props}</h2>*/}
+        <h2>{props.title}</h2> 
+        {/*When we chance the .title to .name we need to chance de name of attribute in App.js line 30 */}
+
+       {/**The way to adapted props instead of expenseAmount.toISOString */}
+       <div className="expense-item__price">${props.amount}
+            </div>
+          </div>
+        </div>
      </div>
     );
 }
 
 export default ExpenseItem;
+
+/** We use props all the time when we building a app */

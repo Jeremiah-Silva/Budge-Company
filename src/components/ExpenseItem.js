cd7ad23607579ebe26baa122fbd669a5a1e78327
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-06-30 13:26:24 -0600

    Applying props.date in the project

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,29 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+    /** Month - props.date.toLocaleString(we can put two arguments - 1) Language 2)Date should be formated "  ")
+     We can define the size of the month display using for example 'long'*/
+    const month = props.date.toLocaleString('en-US', { month: 'long' })
+    /** How to setup the day of month propety */
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' })
+    /** How to setup the year with 4 digits */
+    const year = props.date.getFullYear()
+
 
  //Aplying a dynaminc written in JavaScript
   return (
     <div>
       <div className="expense-item">
-
-       {/**The way to adapted props instead of expenseDate.toISOString */}
-       <div>{props.date.toISOString()} </div>
+      {/**The way to adapted props instead of expenseDate.toISOString */}
+      {/** <div>{props.date.toISOString()} </div>*/}
+      
+      {/** To maintaing the code cleaned we need to put it on const and apply this function to our <div>  */}
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+    
+      {/** developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString */}
+      
 
        <div className="expense-item__description">  
 

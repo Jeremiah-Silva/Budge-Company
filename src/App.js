3cefd0ffac3206cfb73ebc65b4affa2fcdf97e75
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-06-30 21:36:01 -0600

    Updating file and wrote some notes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import Expenses from './components/Expenses';
 function App() {
   const expenses = [
     {
+      // Properties are the values associated with a JavaScript object.
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { 
+      // Properties are the values associated with a JavaScript object.
+      id: 'e2',
+      title: 'New TV', 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12) 
+    },
     {
+      // Properties are the values associated with a JavaScript object.
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
+      // Properties are the values associated with a JavaScript object.
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
@@ -32,6 +41,23 @@ function App() {
 
 export default App;
 
-//There is no a hard rule how to split.
-//Props are our way to passing data from component A to B. 
-//pass data for our component to direct child component.
+/**
+--There is no a hard rule how to split.
+--Props are our way to passing data from component A to B. 
+--Pass data for our component to direct child component.
+*/
+
+/** Months:
+ * 0 = January
+ * 1 = February
+ * 2 = March
+ * 3 = April 
+ * 4 = May
+ * 5 = June
+ * 6 = July
+ * 7 = August 
+ * 8 = September
+ * 9 = October
+ * 10 = November
+ * 11 = December
+ */

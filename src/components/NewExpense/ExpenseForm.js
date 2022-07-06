1e8f6c5792ccd8baf66c8f25300c82ea8ac1a84f
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-07-06 14:22:56 -0600

    Working With Multiples States

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,26 +1,75 @@
-import React from "react";
+//!Store the value and make sure it's "survives" functions can be works property! 
+import React, { useState } from "react";
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-    const titleChangeHandler = (event) => {
-      console.log(event.target.value);  
+    
+  //* These three const is read separetaly. 
+
+    //! a) First const: Title
+    //const [enteredTitle, setEnteredTitle] = useState(''); //*Inicial string!  
+    //! b) Second const: Amount
+    //const [enteredAmount, setEnteredAmount] = useState(''); 
+    //! c) Third contst: 
+    //const [enteredDate, setEntered] = useState(''); 
+    
+  //* Create one object with these three const.
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: '',
+  });
+
+    // a) 1. First Function:
+    const titleChangeHandler = (event) => { 
+      // The lines; 11 and 29 works together.
+      // console.log(event.target.value); 
+      //?            (Argument content to execute "setEnteredTitle");
+      //! setEnteredTitle(event.target.value);
+      setUserInput({
+        ...userInput,
+        enteredTitle: event.target.value
+      })
     };
  
-   return ( 
+    // b) 2.Second Function:
+    const amountChangeHandler = (event) => {
+      // The lines; 37 and 62 works together.
+      // console.log(event.target.value); 
+      //?            (Argument content to execute "setEnteredAmount");
+      //! setEnteredAmount(event.target.value);    
+      setUserInput({
+        ...userInput,
+        enteredAmount: event.target.value
+      })
+    };
+    
+    // c) 3.Third Function:
+    const dateChangeHandler = (event) => {
+      // The lines; 49 and 72 works together.
+      // console.log(event.target.value); 
+      //?            (Argument content to execute "setEnteredDate");
+      //! setEnteredDate(event.target.value);
+      setUserInput({
+      ...userInput,
+      enteredDate: event.target.value
+    })
+  };
+    return ( 
     <form>
         <div className="new-expense__controls">
             <div className="new-expense__control">
               <lable>Title</lable>
-              <input type="text" onChange={titleChangeHandler} />
+              <input type="text" onChange={titleChangeHandler} /> {/** LISTENER {titleChangeHandler} */}
             </div>
             <div className="new-expense__control">
               <lable>Amount</lable>
-              <input type='number' min="0.01" step="0.01" />
+              <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler} /> {/** LISTENER {amountChangeHandler} */}
             </div> 
             <div className="new-expense__control">
               <lable>Date</lable>
-              <input type='date' min="2022-01-01" max="2025"  />  
+              <input type='date' min="2022-01-01" max="2025" onChange={dateChangeHandler} /> {/** LISTENER {dateChangeHandler} */}
             </div>       
         </div>
         <div className="new-exepense__actions">
